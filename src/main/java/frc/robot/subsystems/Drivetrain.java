cea0ce30ed93f47be8ba91214c897428437cdf8d
chem7397 <chem7397@lgsstudent.org>
2022-11-09 16:28:42 -0800

    Odometry stuff
    not done almost done

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -8,7 +8,17 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.kauailabs.navx.frc.AHRS;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
 import frc.robot.util.MotorFactory;
@@ -21,7 +31,14 @@ public class Drivetrain extends SubsystemBase {
   
   private final WPI_TalonFX m_rightMotor1;
   private final WPI_TalonFX m_rightMotor2;
-
+  DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(Units.inchesToMeters(26.0));
+  ChassisSpeeds chassisSpeeds = new ChassisSpeeds(2.0, 0, 1.0);
+  DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
+  AHRS ahrs = new AHRS(SPI.Port.kMXP);
+  DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(ahrs.getRotation2d());
+  Pose2d m_pose2d = new Pose2d(1,1,ahrs.getRotation2d());
+  double leftVelocity = wheelSpeeds.leftMetersPerSecond;
+  double rightVelocity = wheelSpeeds.rightMetersPerSecond;
   public Drivetrain() {
     m_leftMotor1 = MotorFactory.createTalonFX(Constants.drive.kLeftMotor1);
     m_leftMotor2 = MotorFactory.createTalonFX(Constants.drive.kLeftMotor2);
@@ -30,6 +47,7 @@ public class Drivetrain extends SubsystemBase {
 
     m_leftMotor2.follow(m_leftMotor1);
     m_rightMotor2.follow(m_rightMotor1);
+    
   }
 
   /**
@@ -38,7 +56,14 @@ public class Drivetrain extends SubsystemBase {
    * @param leftPower the commanded power to the left motors
    * @param rightPower the commanded power to the right motors
    */
-  public void tankDrive(double leftPower, double rightPower) {
+  @Override
+   public void periodic(){
+    var gyroAngle = Rotation2d.fromDegrees(ahrs.getAngle());
+    //need encoder values to meter
+    // m_pose = m_odometry.update(gyroAngle, m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+
+  }
+   public void tankDrive(double leftPower, double rightPower) {
     m_leftMotor1.set(ControlMode.PercentOutput, leftPower);
     m_rightMotor1.set(ControlMode.PercentOutput, rightPower);
   }
@@ -52,5 +77,12 @@ public class Drivetrain extends SubsystemBase {
   public void arcadeDrive(double throttle, double turn) {
     m_leftMotor1.set(ControlMode.PercentOutput, throttle + turn);
     m_rightMotor1.set(ControlMode.PercentOutput, throttle - turn);
+  }
+  public double getleftEncoderdistanceMeters(){
+
+    double distanceinches=m_leftMotor1.getSelectedSensorPosition()*2048*12/62*4;
+    return Units.inchesToMeters(distanceinches);
+
+
   }
 }
