994e6ad3e6ac0eb880bc5285026dae4ac5cd63e3
johaj10 <78065218+johaj10@users.noreply.github.com>
2022-11-18 16:18:51 -0800

    Disable drivetrain for testing

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -2,9 +2,9 @@ package frc.robot.constants;
 
 public class DriveConstants {
 
-  public final int kLeftMotor1 = -1;
-  public final int kLeftMotor2 = -1;
-  public final int kRightMotor1 = -1;
-  public final int kRightMotor2 = -1;
+  public final int kLeftMotor1 = 15;
+  // public final int kLeftMotor2 = -1;
+  public final int kRightMotor1 = 16;
+ //  public final int kRightMotor2 = -1;
   
 }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -17,19 +17,19 @@ import frc.robot.util.MotorFactory;
 public class Drivetrain extends SubsystemBase {
 
   private final WPI_TalonFX m_leftMotor1;
-  private final WPI_TalonFX m_leftMotor2;
+  // private final WPI_TalonFX m_leftMotor2;
   
   private final WPI_TalonFX m_rightMotor1;
-  private final WPI_TalonFX m_rightMotor2;
+  // private final WPI_TalonFX m_rightMotor2;
 
   public Drivetrain() {
     m_leftMotor1 = MotorFactory.createTalonFX(Constants.drive.kLeftMotor1);
-    m_leftMotor2 = MotorFactory.createTalonFX(Constants.drive.kLeftMotor2);
+    // m_leftMotor2 = MotorFactory.createTalonFX(Constants.drive.kLeftMotor2);
     m_rightMotor1 = MotorFactory.createTalonFX(Constants.drive.kRightMotor1);
-    m_rightMotor2 = MotorFactory.createTalonFX(Constants.drive.kRightMotor2);
+    // m_rightMotor2 = MotorFactory.createTalonFX(Constants.drive.kRightMotor2);
 
-    m_leftMotor2.follow(m_leftMotor1);
-    m_rightMotor2.follow(m_rightMotor1);
+    // m_leftMotor2.follow(m_leftMotor1);
+    // m_rightMotor2.follow(m_rightMotor1);
   }
 
   /**
