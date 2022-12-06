3b49e0d1d76df0e22409e17738f1782dbd5b6366
johaj10 <78065218+johaj10@users.noreply.github.com>
2022-12-05 16:15:30 -0800

    Update Constants.java

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -7,9 +7,9 @@ public final class Constants {
   public static final OIConstants oi = new OIConstants();
   public static final DriveConstants drive = new DriveConstants();
   public static final AutoConstants auto = new AutoConstants();
-  public static final int OuttakePort = -1;
+  public static final int OuttakePort = 11;
   public static final int IntakePort = 3;
-  public static final int ConveyorPort = -1;
+  public static final int ConveyorPort = 10;
 
 
 }
