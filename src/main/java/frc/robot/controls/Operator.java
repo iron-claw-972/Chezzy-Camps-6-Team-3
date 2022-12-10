96997341f40aa145c1d75c406c1effaa5fd108da
johaj10 <78065218+johaj10@users.noreply.github.com>
2022-12-09 16:23:08 -0800

    Updated shuffleboard

--- src/main/java/frc/robot/controls/Operator.java
+++ src/main/java/frc/robot/controls/Operator.java
@@ -3,6 +3,7 @@ package frc.robot.controls;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.DoNothing;
 import frc.robot.constants.Constants;
+import frc.robot.util.ShuffleboardManager;
 import frc.robot.Robot;
 import lib.controllers.GameController;
 import lib.controllers.GameController.Button;
@@ -11,8 +12,16 @@ public class Operator {
   private static GameController operator = new GameController(Constants.oi.kOperatorJoy);
 
   public static void configureControls() {
-    operator.get(Button.A).whenPressed(new InstantCommand(() -> Robot.conveyor.setMotor(0.1)));
-    operator.get(Button.A).whenReleased(new InstantCommand(() -> Robot.conveyor.setMotor(0)));
+    operator.get(Button.A).whenPressed(new InstantCommand(() -> {
+      Robot.conveyor.setMotor(0.1);
+      // Robot.shuffleboard.setIntakeStatus(true);
+    
+    }));
+    Robot.shuffleboard.setIntakeStatus(true);
+    operator.get(Button.A).whenReleased(new InstantCommand(() -> {
+      Robot.conveyor.setMotor(0);
+      // Robot.shuffleboard.setIntakeStatus(false);
+    }));
   }
 
 }
--- src/main/java/frc/robot/util/ShuffleboardManager.java
+++ src/main/java/frc/robot/util/ShuffleboardManager.java
@@ -1,5 +1,7 @@
 package frc.robot.util;
 
+import java.util.function.BooleanSupplier;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -7,7 +9,10 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.Robot;
+import frc.robot.subsystems.Intake;
 
 public class ShuffleboardManager {
 
@@ -17,13 +22,18 @@ public class ShuffleboardManager {
   ShuffleboardTab m_autoTab = Shuffleboard.getTab("Auto");
 
   NetworkTableEntry m_commandScheduler = m_mainTab.add("Command Scheduler", "NULL").getEntry();
-  
+
   public void setup() {
     LiveWindow.disableAllTelemetry(); // LiveWindow is causing periodic loop overruns
 
     chooserUpdate();
 
     m_autoTab.add("Auto Chooser", m_autoCommand);
+
+    m_mainTab.add("Start Intake", new InstantCommand(() -> Robot.intake.setMotor(0.1)));
+    m_mainTab.add("Stop Intake", new InstantCommand(() -> Robot.intake.setMotor(0.0)));
+    
+    // m_mainTab.addBoolean("Intake running: ", setIntakeStatus(intakeOn));
   }
 
   public Command getAutonomousCommand() {
@@ -34,10 +44,21 @@ public class ShuffleboardManager {
     m_autoCommand.addOption("Do Nothing", new PrintCommand("This will do nothing!"));
   }
 
+  public BooleanSupplier setIntakeStatus(boolean intakeOn) {
+    return () -> intakeOn;
+  }
+
+  public void setOuttakeStatus(boolean outtakeOn) {
+    BooleanSupplier outtakeStatus = () -> outtakeOn;
+
+    m_mainTab.addBoolean("Outtake running: ", outtakeStatus);
+  }
+
   public void loadCommandSchedulerShuffleboard(){
     CommandScheduler.getInstance().onCommandInitialize(command -> m_commandScheduler.setString(command.getName() + " initialized."));
     CommandScheduler.getInstance().onCommandInterrupt(command -> m_commandScheduler.setString(command.getName() + " interrupted."));
     CommandScheduler.getInstance().onCommandFinish(command -> m_commandScheduler.setString(command.getName() + " finished."));
   }
+
 
 }
